24d74104f34603149dd3dffb612f5bc958ea7d6b
Yuje <dbwp031@gmail.com>
2025-03-03 15:20:41 +0900

    first commit

---
+++ java-transaction/src/main/java/code/ground/javatransaction/JavaTransactionApplication.java
@@ -0,0 +1,13 @@
+package code.ground.javatransaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaTransactionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaTransactionApplication.class, args);
+    }
+
+}
---
+++ java-transaction/src/test/java/code/ground/javatransaction/JavaTransactionApplicationTests.java
@@ -0,0 +1,13 @@
+package code.ground.javatransaction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaTransactionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
