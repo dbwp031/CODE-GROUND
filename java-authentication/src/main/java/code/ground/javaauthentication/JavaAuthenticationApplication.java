196c9975f5afd520b77d603ab5c8f062bc0ed679
yj.lee <dbwp031@gmail.com>
2025-03-14 20:46:01 +0900

    initial commit

---
+++ java-authentication/src/main/java/code/ground/javaauthentication/JavaAuthenticationApplication.java
@@ -0,0 +1,13 @@
+package code.ground.javaauthentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaAuthenticationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaAuthenticationApplication.class, args);
+    }
+
+}
---
+++ java-authentication/src/test/java/code/ground/javaauthentication/JavaAuthenticationApplicationTests.java
@@ -0,0 +1,13 @@
+package code.ground.javaauthentication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaAuthenticationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
